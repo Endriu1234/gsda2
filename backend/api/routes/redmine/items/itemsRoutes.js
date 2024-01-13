95c2da55b5ff9b10969d493d32656eeb984dbf82
Andrzej Czaja <ajm.czaja@gmail.com>
2024-01-13 16:30:24 +0100

    item creation attachment part 2

--- backend/api/routes/redmine/items/itemsRoutes.js
+++ backend/api/routes/redmine/items/itemsRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/get-redmine-trackers', authController.checkAuth, itemsController.getRedmineTrackers);
 router.get('/get-redmine-users', authController.checkAuth, itemsController.getRedmineUsers);
 router.get('/get-redmine-projects', authController.checkAuth, itemsController.getRedmineProjects);
-router.post('/create-redmine-item', authController.checkAuth, fileStorage.getFileStorage().array('files', 10), itemsController.createRedmineItem);
+router.post('/create-redmine-item', authController.checkAuth, fileStorage.getFileStorage().array('files', process.env.USER_FILES_LIMIT), itemsController.createRedmineItem);
 router.get('/get-potential-redmine-items-from-rmproject', authController.checkAuth, itemsController.getPotentialRedmineItemsFromRedmineProject);
 router.get('/get-redmine-versions', authController.checkAuth, itemsController.getRedmineVersions);
 router.post('/save-redmine-attachement', authController.checkAuth, itemsController.saveRedmineAttachement);
