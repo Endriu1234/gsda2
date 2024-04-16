3cf68dd9576137d8fb1e4daf427c96657ac41460
Andrzej Czaja <ajm.czaja@gmail.com>
2024-04-16 19:00:57 +0200

    Prod preparation

--- backend/configuration/app_configuration/appConfigurationLoader.js
+++ backend/configuration/app_configuration/appConfigurationLoader.js
@@ -13,6 +13,7 @@ function configurePATH() {
 }
 
 async function connectToMongo() {
+    mongoose.set('strictQuery', false);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', () => { console.log("Database connected") });
